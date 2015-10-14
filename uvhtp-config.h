60345c5f3024f1b751ec97cb1540413f037b3e63
smart__petea <smart__petea@mail.ru>
2015-10-14 20:53:13 +0300

    ...

---
+++ uvhtp-config.h
@@ -0,0 +1,13 @@
+#ifndef __UVHTP_CONFIG_H__
+#define __UVHTP_CONFIG_H__
+
+#ifdef __cplusplus
+extern "C" {
+}
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ uvhtp.h
@@ -0,0 +1,6 @@
+#include "uvhtp-config.h"
+
+#ifndef __UVHTP__H_
+#define __UVHTP__H_
+#endif
+
